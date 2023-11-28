adae6acc6631fe721e12ceb089717c237718c9e9
grouvimsh <151635542+grouvimsh@users.noreply.github.com>
2023-11-28 20:34:36 +0700

    Add files via upload

---
+++ Kontrolnaya5zadanie.cpp
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int** A, **B, **C;
+int size;
+std::cout << "Input size"; std::cin >> size;
+
+A = new int* [size];
+B = new int* [size];
+C = new int* [size];
+
+for (int i=0; i<size; i++) {
+A[i] = new int[size];
+B[i] = new int[size];
+C[i] = new int[size];
+
+}
+
+for (int i=0; i<size; i++) {
+for (int j=0; j<size; j++) {
+std::cin>>A[i][j];
+}
+}
+
+for (int i=0; i<size; i++) {
+for (int j=0; j<size; j++) {
+std::cin>>B[i][j];
+}
+}
+
+
+for (int i=0; i<size; i++) {
+for (int j=0; j<size; j++) {
+C[i][j] = A[i][j] + B[i][j];
+}
+}
+
+std::cout << "result:" << std::endl;
+for (int i=0; i<size; i++) {
+for (int j=0; j<size; j++) {
+std::cout << C[i][j] << ' ';
+}
+std::cout << std::endl;
+}
+std::cout << "End";
+
+for (int i=0; i<size; i++) {
+    delete [] A[i];
+    delete [] B[i];
+    delete [] C[i];
+    }
+delete [] A;
+delete [] B;
+delete [] C;
+}
